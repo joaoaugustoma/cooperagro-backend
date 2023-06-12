5e73cd9d80ffe6028d556f8faf83f31a2240e6e9
João Augusto Moreira Ananias <joao.ananias@aluno.ueg.br>
2023-06-12 15:27:20 -0300

    Add files via upload

---
+++ src/test/java/com/cooperagro/backend/CooperagroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooperagro.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperagroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
